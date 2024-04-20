fa63c00bea415ecedb36a5da5aa43a84a19813a1
TBS-studio <154610109+TBS-studio@users.noreply.github.com>
2024-04-20 20:40:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-from从烧瓶进口*import *
-from来自泡菜进口*import *
-应用程序=烧瓶（__名称__）Flask(__name__)
+from flask import *
+from pickle import *
+app = Flask(__name__)
 
-@@app.route('/',methods=["get","post"])('/',methods=["get","post"])
-defdef index(): # 将应用程序的代码放在这里index():  # put application's code here
-    返回 render_template("index.html",error="")return render_template("index.html",error="")
-    user=request.values.get("用户")values.get("user")
-    pwd=request.values.get("pwd")values.get("pwd")
-    如果用户不在用户中：if user not in users:
-        return render_template("index.html",error="用户名不存在")return render_template("index.html",error="用户名不存在")
-    elif 用户[用户] != pwd:elif users[user] != pwd:
-        return render_template("index.html",error="密码不正确")return render_template("index.html",error="密码不正确")
-    别的：else:
-        会话[“用户”]=用户["user"]=user
-        session.permanent=Truepermanent=True
-        返回 render_template("index.html")return render_template("index.html")
+@app.route('/',methods=["get","post"])
+def index():  # put application's code here
+    return render_template("index.html",error="")
+    user=request.values.get("user")
+    pwd=request.values.get("pwd")
+    if user not in users:
+        return render_template("index.html",error="用户名不存在")
+    elif users[user] != pwd:
+        return render_template("index.html",error="密码不正确")
+    else:
+        session["user"]=user
+        session.permanent=True
+        return render_template("index.html")
     
-try尝试：
-    以 open("users.pickle","rb") 作为 f：with open("users.pickle","rb") as f:
-        用户=负载（f）load(f)
-except除了：
-    用户={}{}
+try: 
+    with open("users.pickle","rb") as f:
+        users = load(f)
+except:
+    users={}
     
 
     
-app.route("/登录",methods=["get","post"])route("/Login",methods=["get","post"])
-defdef 登录():Login():
-    user=request.values.get("用户")values.get("user")
-    pwd=request.values.get("pwd")values.get("pwd")
-    如果用户不在用户中：if user not in users:
-        return render_template("index.html",error="用户名不存在")return render_template("index.html",error="用户名不存在")
-    elif 用户[用户] != pwd:elif users[user] != pwd:
-        return render_template("index.html",error="密码不正确")return  render_template  (  "index.html" ,error= "密码不正确"  )
-    另外：
-        会话[ “用户” ] = 用户
-        session.永久=真
-        返回渲染模板（“index.html”）html”）
+app.route("/Login",methods=["get","post"])
+def Login():
+    user=request.values.get("user")
+    pwd=request.values.get("pwd")
+    if user not in users:
+        return render_template("index.html",error="用户名不存在")
+    elif users[user] != pwd:
+        return render_template("index.html",error="密码不正确")
+    else:
+        session["user"]=user
+        session.permanent=True
+        return render_template("index.html")
     
     
-应用程序跑步（）
+app.run()
